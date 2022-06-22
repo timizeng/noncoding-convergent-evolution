bfd9104ca64cd6ca5e155299a11ca42649c3f614
timizeng <timizeng@users.noreply.github.com>
2022-06-22 10:37:16 +0800

    Update stepbystep.phast.sh

--- stepbystep.phast.sh
+++ stepbystep.phast.sh
@@ -1,4 +1,3 @@
-#一键生成脚本
 #step1 长度筛选，只保留基因组中大于10000bp的序列
 for i in *.fa;
 do 
@@ -121,10 +120,13 @@ for i in *.sing.maf; do base=$(basename ${i} .arabidopsis_thaliana.sing.maf); mv
 #step8:combine multiple alignment
 roast + E=arabidopsis_thaliana "(((arabidopsis_thaliana theobroma_cacao) (citrus_sinensis (swietenia_macrophylla (xylocarpus_granatum (xylocarpus_rumphii xylocarpus_moluccensis))))) (populus_trichocarpa (carallia_pectinifolia ((bruguiera_gymnorhiza bruguiera_sexangula) ((ceriops_tagal (kandelia_obovata kandelia_candel)) (rhizophora_mangle (rhizophora_apiculata (rhizophora_mucronata rhizophora_stylosa))))))))" *.*.sing.maf 18species.final.maf
 
+.................................以下开始phast流程.............................
 ####phast
 #使用 mafSplit 分解比对文件,以arabidopsis_thaliana染色体为单位分解
 mafSplit -byTarget arabidopsis_thaliana -useFullSequenceName mafplit/ 18species.final.maf
 
+
+...........................提取4d位点估计单条染色体non.conserved.mod.....................
 #提取4d位点估计 non.conserved.mod
 for i in Chr1 Chr2 Chr3 Chr4 Chr5 ChrM ChrC; 
 do 
@@ -136,10 +138,10 @@ do
     msa_view ${i}.4d-codons.ss --in-format SS --out-format SS --tuple-size 1 > ${i}.4d-sites.ss
 done
 
-#
+#合并所有染色体4d位点
 msa_view --unordered-ss --out-format SS --aggregate arabidopsis_thaliana,theobroma_cacao,citrus_sinensis,swietenia_macrophylla,xylocarpus_granatum,xylocarpus_rumphii,xylocarpus_moluccensis,populus_trichocarpa,carallia_pectinifolia,bruguiera_gymnorhiza,bruguiera_sexangula,ceriops_tagal,kandelia_obovata,kandelia_candel,rhizophora_mangle,rhizophora_apiculata,rhizophora_mucronata,rhizophora_stylosa *sites.ss > all.4d-sites.ss
 
-#总体的non.cons.mod
+#估计总体的non.cons.mod
 phyloFit --tree "(((arabidopsis_thaliana,theobroma_cacao),(citrus_sinensis,(swietenia_macrophylla,(xylocarpus_granatum,(xylocarpus_rumphii,xylocarpus_moluccensis))))),(populus_trichocarpa,(carallia_pectinifolia,((bruguiera_gymnorhiza,bruguiera_sexangula),((ceriops_tagal,(kandelia_obovata,kandelia_candel)),(rhizophora_mangle,(rhizophora_apiculata,(rhizophora_mucronata,rhizophora_stylosa))))))))" --msa-format SS --out-root nonconserved-4d all.4d-sites.ss
 
 #计算单条染色体的non.cons.mod
@@ -150,6 +152,7 @@ do
 done
 
 
+.......................通过estimate-rho分别计算单条染色体的con.elements....................
 ##通过estimate-rho和每条染色体4d位点估计出的noncons.mod来估算每条染色体的cons.mod
 for i in *.maf;
 do 
@@ -173,7 +176,6 @@ done
 chmod 755 con.element.sh
 ./con.element.sh
 
-
 ......该方法出现警告.....如下：
 Finding MLE for (rho)...
 Computing emission probs (state 1, cat 1, mod 1)...
@@ -181,6 +183,7 @@ WARNING: likelihood decreased during EM: it 8 total_logl=-580783.8808, prev_tota
 (rho = 0.199658)
 
 
+.................通过滑窗和 --estimate-trees 计算滑窗的cons.mod 和 non.cons.mod 最后对单个染色体聚合mods......................
 ##通过滑窗计算 每条 染色体的 cons.mod
 #根据 http://compgen.cshl.edu/phast/phastCons-HOWTO.html 中的4.1部分教程重新分析
 1.Split the alignments into small fragments
@@ -198,7 +201,7 @@ mkdir -p chunks            # put fragments here
 mkdir -p trees     # put estimated tree models here
 rm -f trees/*      # in case old versions left over
 
-#每个染色体逐条运行
+#以4d位点得到的对应染色体non.cons.mod作为输入，每个染色体逐条运行
 for file in chunks/Chr1.*.ss ; 
 do 
     root=$(basename ${file} .ss)  
